2d001742b61ca945484c4620be3ebf7d3ba47df3
Morteza <morteza.sharifi.1995@gmail.com>
2024-09-16 20:05:46 -0400

    generate token with user read only

--- src/test/java/tek/tdd/api/test/TokenGenerationTest.java
+++ src/test/java/tek/tdd/api/test/TokenGenerationTest.java
@@ -3,6 +3,8 @@ package tek.tdd.api.test;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.annotations.Test;
 import tek.tdd.base.ApiTestsBase;
 
@@ -10,6 +12,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TokenGenerationTest extends ApiTestsBase {
+    private static final Logger LOGGER = LogManager.getLogger(TokenGenerationTest.class);
 
     @Test
     public void generateTokenWithSupervisor(){
@@ -17,7 +20,16 @@ public class TokenGenerationTest extends ApiTestsBase {
         request.body(RequestBody.supervisorUserRequest());
         Response response = request.when().post(Methods.apiToken);
         response.then().statusCode(200);
-        response.prettyPrint();
+        LOGGER.info("Response is {} ",response.prettyPrint());
+
     }
 
+    @Test
+    public void generateTokenWithReadOnly(){
+        RequestSpecification request = getDefaultRequest();
+        request.body(RequestBody.readOnlyUser());
+        Response response = request.when().post(Methods.apiToken);
+        response.then().statusCode(200);
+        LOGGER.info("Response is {} ",response.prettyPrint());
+    }
 }
